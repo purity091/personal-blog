bbb468148c6829a0f6deb23871aaa5bde5fefae1
Mohamed Lahlah <your-email@example.com>
2026-04-13 01:21:45 +0300

    w

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,7 @@ import react from '@astrojs/react';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import tailwind from '@astrojs/tailwind';
-import vercel from '@astrojs/vercel';
+import vercel from '@astrojs/vercel/serverless';
 
 export default defineConfig({
   site: 'https://lahlah.ai',
